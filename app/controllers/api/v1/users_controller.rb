5e8f225c5a547f438ad0996f46d235c1b58bb9ba
Haiwen <haiwensummer@gmail.com>
2016-01-06 23:40:22 +0800

    Adds the user create endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,30 @@
 class Api::V1::UsersController < ApplicationController
+	skip_before_filter  :verify_authenticity_token
 	respond_to :json
 
 	def show
 		respond_with User.find(params[:id])
 	end
+
+	def index
+		@users = User.all
+		render json:  { :flag => true, :user => @users }
+		#respond_to do |format|
+		#	format.json { render json: @users }
+		#end
+	end
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			render json: @user, status: 201, location: [:api, @user]
+		else
+			render json: { errors: @user.errors}, status: 422
+		end
+	end
+
+	private
+		def user_params
+			params.require(:user).permit(:username, :email, :password, :password_confirmation)
+		end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ require 'api_constraints'
 MarketPlaceApi::Application.routes.draw do
   #mount SabisuRails::Engine => "/sabisu_rails"
   devise_for :users
-  namespace :api, defaults: { format: :json }, 
-                  constraints: { subdomain: 'api'}, path: "/" do
+  #namespace :api, defaults: { format: :json }, 
+  #                constraints: { subdomain: 'api'}, path: "/" do
+  namespace :api, defaults: { format: :json } do
 	scope module: :v1,
 				constraints: ApiConstraints.new(version: 1, default: true) do
-		resources :users, :only => [:show]
+		resources :users
 	end          
 
   end

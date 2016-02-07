0dc6b68ed7718ea554ac0407a8f7c39e5cf3da6a
Haiwen <haiwensummer@gmail.com>
2016-02-07 22:48:29 +0800

    Adds shoulda matchers for spec refactors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,3 +11,13 @@ RSpec.describe User, type: :model do
 
   it { should be_valid }
 end
+
+RSpec.describe "when email is not present" do
+  before { @user.email = " " }
+  it { should_bot be_valid }
+
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { should validate_confirmation_of(:password) }
+  it { should allow_value('example@domain.com').for(:email) }
+end
